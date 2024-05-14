d492c5c40d1c82d8b206946353b4b073c92aa041
aliev_a <aliev_a@etna-alternance.net>
2024-05-14 14:32:01 +0200

    fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 from tqdm import tqdm
@@ -14,8 +16,18 @@ FORM_URL = "https://careers.thalesgroup.com/fr/fr/apply?jobSeqNo=TGPTGWGLOBALR02
 def fill_form_with_data(browser, entry):
     print("Filling form with data...")
     # print("Browser:", browser)
+    
+    wait = WebDriverWait(browser, 10)
+
+    try:
+        accept_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[text()='Accepter']")))
+        accept_button.click()
+    except Exception as e:
+        print("Error clicking accept button:", e)
+        
     browser.find_element(By.ID, "sourceType").send_keys(
         entry['Comment avez-vous entendu parler de nous ?'])
+    sleep(1)
     browser.find_element(By.ID, "applicantSource").send_keys(entry['Source'])
     browser.find_element(By.ID, "country").send_keys(entry['Pays'])
     browser.find_element(
@@ -28,8 +40,10 @@ def fill_form_with_data(browser, entry):
         entry['Nom et type de voie ou/ Adresse'])
     browser.find_element(By.ID, "cntryFields.regionReference").send_keys(
         entry['Département'])
+    sleep(1)
     browser.find_element(
         By.ID, "cntryFields.cityReference").send_keys(entry['Ville'])
+    sleep(2)
     browser.find_element(By.ID, "cntryFields.postalCode").send_keys(
         str(entry['Code postal']))
     browser.find_element(By.ID, "email").send_keys(
@@ -40,6 +54,8 @@ def fill_form_with_data(browser, entry):
         entry['Indicatif téléphonique du pays'])
     browser.find_element(By.ID, "phoneWidget.phoneNumber").send_keys(
         str(entry['Numéro de téléphone']))
+    # browser.find_element(By.ID ,"next").click()
+    browser.find_element(By.ID, "privacyCheckBox").click()
 
 
 def fill_form_from_dataframe(browser, df):
@@ -61,7 +77,7 @@ def launch_browser_and_fill_form():
     
     browser.get(FORM_URL)
     while True:
-        sleep(2)
+        sleep(5)
         fill_form_from_dataframe(browser, df)
         return browser
 
